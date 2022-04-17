7eeb2adefc40b436fcdf9d1b5ead9a07f325df5b
gudals95 <102935090+gudals95@users.noreply.github.com>
2022-04-17 15:30:01 +0900

    feature/이벤트 핸들러 추가/제거 관리추가

--- build/utils/getPosition.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getPosition = void 0;
-var getPosition = function (event, pos) {
-    pos.x = event.pageX;
-    pos.y = event.pageY;
-};
-exports.getPosition = getPosition;
-//# sourceMappingURL=getPosition.js.map
